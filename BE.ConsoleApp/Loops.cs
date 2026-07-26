d8d36aa6a9102f8b7fa14890d0710f2e430b7258
xcuongcs5 <xcuongcs50@gmail.com>
2026-07-26 16:32:04 +0700

    data type, conditional, loop

---
+++ BE.ConsoleApp/Loops.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+        
+    }
+}
--- BE.ConsoleApp/Program.cs
+++ BE.ConsoleApp/Program.cs
@@ -10,15 +10,48 @@ namespace BE.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string name = "Xuan Cuong";
+            //Console.Write("Enter a month (1-12): ");
+            //int month = int.Parse(Console.ReadLine());
+            //int days;
 
-            var name2 = "Charlie";
+            //switch(month)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        days = 31;
+            //        break;
+            //    case 4:
+            //    case 6:
+            //    case 9:
+            //    case 11:
+            //        days = 30;
+            //        break;
+            //    case 2:
+            //        days = 28;
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid month");
+            //        return;
+            //}
+            //Console.WriteLine("Number of days in month {0}: {1}", month, days);
 
-            var name3 = 2002;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine("i = {0}", i);
+            }
 
-            int num = 10
-
-            Console.WriteLine("Hello, {0}", name);
+            //Console.WriteLine("Nhan phim bat ky de thoat...");
+            Console.WriteLine("Nhấn phím bất kỳ để thoát...");
+            Console.ReadKey();
         }
     }
 }
